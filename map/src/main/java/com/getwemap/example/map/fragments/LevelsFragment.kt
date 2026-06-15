6626581ecdc0c861c1772f7db1b7572d590d87a1
Evgenii Khrushchev <eugene@getwemap.com>
2026-06-15 17:23:11 +0200

    add an example of how to draw a circle around a coordinate

--- map/src/main/java/com/getwemap/example/map/fragments/LevelsFragment.kt
+++ map/src/main/java/com/getwemap/example/map/fragments/LevelsFragment.kt
@@ -1,5 +1,6 @@
 package com.getwemap.example.map.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,15 @@ import com.getwemap.sdk.core.internal.geo.LevelUtils
 import com.getwemap.sdk.core.internal.helpers.Logger
 import com.getwemap.sdk.core.model.entities.PointOfInterest
 import org.maplibre.android.MapLibre
+import org.maplibre.android.maps.MapLibreMap
+import org.maplibre.android.maps.Style
+import org.maplibre.android.style.layers.LineLayer
+import org.maplibre.android.style.layers.Property
+import org.maplibre.android.style.layers.PropertyFactory
+import org.maplibre.android.style.sources.GeoJsonSource
+import org.maplibre.geojson.Point
+import org.maplibre.turf.TurfConstants
+import org.maplibre.turf.TurfTransformation
 
 class LevelsFragment : MapFragment() {
 
@@ -36,8 +46,9 @@ class LevelsFragment : MapFragment() {
         buttonFirstPOI.setOnClickListener { firstClicked() }
         buttonSecondPOI.setOnClickListener { secondClicked() }
 
-        mapView.getMapViewAsync { mapView, map, style, data ->
+        mapView.getMapViewAsync { _, map, style, _ ->
             uniqueLevels = pois.mapNotNull { it.coordinate.levels.firstOrNull() }.toSet()
+            drawCircleAroundCenter(map, style)
         }
     }
 
@@ -62,8 +73,35 @@ class LevelsFragment : MapFragment() {
         pointOfInterestManager.selectPOI(randomPOI)
     }
 
+    private fun drawCircleAroundCenter(map: MapLibreMap, style: Style) {
+        style.getLayer(CIRCLE_LAYER_ID)?.let { style.removeLayer(it) }
+        style.getSource(CIRCLE_SOURCE_ID)?.let { style.removeSource(it) }
+
+        val center = map.cameraPosition.target ?: return
+        val circle = TurfTransformation.circle(
+            Point.fromLngLat(center.longitude, center.latitude),
+            CIRCLE_RADIUS_METERS, CIRCLE_VERTICES, TurfConstants.UNIT_METERS
+        )
+
+        style.addSource(GeoJsonSource(CIRCLE_SOURCE_ID, circle))
+        style.addLayer(
+            LineLayer(CIRCLE_LAYER_ID, CIRCLE_SOURCE_ID).withProperties(
+                PropertyFactory.lineColor(Color.RED),
+                PropertyFactory.lineWidth(2f),
+                PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND)
+            )
+        )
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val CIRCLE_SOURCE_ID = "center-circle-source"
+        private const val CIRCLE_LAYER_ID = "center-circle-layer"
+        private const val CIRCLE_RADIUS_METERS = 100.0
+        private const val CIRCLE_VERTICES = 64
     }
 }
